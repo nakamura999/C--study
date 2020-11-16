1a4808815590621ec9081845e53971adc58b1964
nakamura999 <masahiro9ama9@gmail.com>
2020-11-16 17:03:06 +0900

    Class-sansyo problem1 add

---
+++ Classの相互参照/problem1/main.cpp
@@ -0,0 +1,15 @@
+#include "foo.h"
+#include "bar.h"
+#include <iostream>
+ 
+using namespace std;
+ 
+int main(){
+    Foo* pFoo;
+    Bar* pBar;
+    pFoo->hoge();
+    pBar->func2();
+    pFoo->fuga(pBar);
+    pBar->func1(pFoo);
+    return 0;
+}
