3ea84f32d4a36405bb6b07a4af2cb400f3c97099
nakamura999 <masahiro9ama9@gmail.com>
2020-11-23 21:26:04 +0900

    STL2 problem7 edit

--- STL2/problem7.cpp
+++ STL2/problem7.cpp
@@ -1,23 +1,27 @@
-#include<iostream>
-#include<stack>
+#include <iostream>
+#include <stack>
+#include <string>
+
 using namespace std;
+
 int main() {
-  stack<char> st;
-  char c;
-  cin >> c;
+    stack<char> st;
+    char c[100];
+    int i = 0;
 
-  while (cin >> c) {
-    if(c != '\0') {
-        st.push(c);
-    } else {
-        break;
+    cout << "英単語を入力 : ";
+    cin >> c;
+
+    while (c[i] != '\0') {
+        st.push(c[i]);
+        i++;
     }
-  }
 
-  while (!st.empty()) {
-    cout << st.top();
-    st.pop();
-  }
-  cout << endl;
-  return 0;
+    cout << "逆表示 : ";
+    while (!st.empty()) {
+        cout << st.top();
+        st.pop();
+    }
+    cout << endl;
+    return 0;
 }
